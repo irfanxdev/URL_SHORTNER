67d7bb881b90d5465cb31052972233fdd5441fb8
irfan ansari <irfa8318450619@gmail.com>
2026-06-22 14:50:14 +0530

    update ui

--- backend/models/url.js
+++ backend/models/url.js
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
 
-const urlSchema = new mongoose.Schema({
-  shortId: { type: String, required: true, unique: true },
-  redirectUrl: { type: String, required: true },
-  visitHistory: [
-    {
-      timestamp: { type: Number },
+const urlSchema = new mongoose.Schema(
+  {
+    shortId: { type: String, required: true, unique: true },
+    redirectUrl: { type: String, required: true },
+    visitHistory: [
+      {
+        timestamp: { type: Number },
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
-  ],
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "user", required: true }
-}, { timestamps: true });
+  },
+  { timestamps: true },
+);
 
 const Url = mongoose.model("Url", urlSchema);
 export default Url;
-
